6c1d81e8e7fc5f03f3e3d7357d7ecf0b9d974bd7
Andrew Tuliszewski <defpearlpilot@yahoo.com>
2020-03-21 19:42:30 -0400

    additional rc files

---
+++ .brewrc.sh
@@ -0,0 +1,5 @@
+which -s brew 1> /dev/null
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
---
+++ .rustrc.sh
@@ -0,0 +1,1 @@
+pathadd $HOME/.cargo/bin
